c458f20c5417aaefe8f502dbded2c32c1cbf1aea
Rutesh <ruteshrathod@gmail.com>
2019-10-07 17:59:02 +0530

    Mutaions & Actions Part 1
    Last Part is still remaining, (submit)

---
+++ src/views/DemoEventCreate.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Create an Event{{ user.name }}</h1>
+    <p>This event is created by {{ user.id }}</p>
+    <p>There are:- {{ catLength }} categories</p>
+    <p>{{ getEventById }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+export default {
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    ...mapGetters(['getEventById']),
+    ...mapState(['user', 'categories'])
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
